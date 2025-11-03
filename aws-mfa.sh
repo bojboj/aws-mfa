a257d97128d76362c0b5f2406fb248d23b17be36
Job Cabral <jcabral@engagednation.com>
2025-11-03 10:38:25 -0800

    Add argument parsing to AWS MFA script
    Implements profile selection via `-p` or `--profile`, provides usage instructions, and validates the selected profile configuration.

--- aws-mfa.sh
+++ aws-mfa.sh
@@ -1,5 +1,39 @@
 #!/bin/bash
 
+PROGRAM_NAME=$(basename "$0")
+
+usage () {
+  echo "$PROGRAM_NAME: usage: $PROGRAM_NAME [-p profile]"
+  return
+}
+
+profile=default
+
+while [[ -n "$1" ]]; do
+  case "$1" in
+    -p | --profile)
+      shift
+
+      if [[ -n "$1" ]]; then
+        profile="$1"
+      fi
+
+      ;;
+    *)
+      usage >&2
+      exit 1
+
+      ;;
+  esac
+  shift
+done
+
+if [ -n "$(aws configure get aws_access_key_id --profile="$profile" 2>/dev/null)" ]; then
+    echo "Using $profile profile."
+else
+    echo "$profile profile is not configured."
+fi
+
 echo -n "Enter your one time token: "
 read token
 
@@ -19,4 +53,3 @@ export AWS_SESSION_TOKEN=$(echo "$credentials" | jq -r ".Credentials.SessionToke
 
 expiration=$(echo "$credentials" | jq -r ".Credentials.Expiration")
 echo "Temporary credentials set. Expires at: $expiration."
-exit 0
